1645402e3162057cdcf4564fa8a66fbbb85f322f
rahachu <13518011@std.stei.itb.ac.id>
2020-03-09 23:06:18 +0700

    initialization

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COVID19_STIMA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
