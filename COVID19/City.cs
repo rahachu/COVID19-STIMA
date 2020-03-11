7719c25d7cf23d9c49d3c8a6e45037e241e9669d
rahachu <49639890+rahachu@users.noreply.github.com>
2020-03-12 02:26:21 +0700

    new data type dengan rapih

---
+++ COVID19/City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID19
+{
+    class City
+    {
+        public Dictionary<string, int> cityCollection;
+        public string start;
+
+        //constructor
+        public City()
+        {
+            cityCollection = new Dictionary<string, int>();
+        }
+
+        public void importCity(string dir)
+        {
+            StreamReader sr = new StreamReader(dir);
+            string[] readed = sr.ReadLine().Split(' ');
+            int n = int.Parse(readed[0]);
+            start = readed[1];s
+            for(int i = 0;i < n; i++)
+            {
+                string[] temp = sr.ReadLine().Split(' ');
+                cityCollection.Add(temp[0], int.Parse(temp[1]));
+            }
+        }
+    }
+}
---
+++ COVID19/CityLogistic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID19
+{
+    class CityLogistic
+    {
+        Dictionary<KeyValuePair<String, String>, float> val;
+
+        public CityLogistic()
+        {
+            val = new Dictionary<KeyValuePair<string, string>, float>();
+        }
+
+        public void importLogistic(string dir)
+        {
+            StreamReader sr = new StreamReader(dir);
+            string ns = sr.ReadLine();
+            int n = Convert.ToInt32(ns);
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] a = sr.ReadLine().Split(' ');
+                KeyValuePair<string, string> cityPair = new KeyValuePair<string, string>(a[0],a[1]);
+                val.Add(cityPair, float.Parse(a[2]));
+            }
+        }
+    }
+}
