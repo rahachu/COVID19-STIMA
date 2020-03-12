61f393706a5c64ec328719453b963ba0119c1927
rahachu <49639890+rahachu@users.noreply.github.com>
2020-03-12 13:55:30 +0700

    add BFS function

--- COVID19/CityLogistic.cs
+++ COVID19/CityLogistic.cs
@@ -9,55 +9,62 @@ namespace COVID19
 {
     class CityLogistic
     {
+        public static Dictionary<string, string[]> cityEdge = new Dictionary<string, string[]>();
         public static Dictionary<KeyValuePair<String, String>, float> val = new Dictionary<KeyValuePair<string, string>, float>();
         public static Dictionary<string, int> timeFirst = new Dictionary<string, int>();
 
 
 
-        public static void importLogistic(string dir)
+        public static void ImportLogistic(string dir)
         {
             StreamReader sr = new StreamReader(dir);
             string ns = sr.ReadLine();
             int n = Convert.ToInt32(ns);
+            timeFirst[City.start] = 0;
 
             for(int i = 0; i < n; i++)
             {
                 string[] a = sr.ReadLine().Split(' ');
                 KeyValuePair<string, string> cityPair = new KeyValuePair<string, string>(a[0],a[1]);
+                cityEdge[a[0]].Append(a[1]);
                 val.Add(cityPair, float.Parse(a[2]));
             }
         }
 
-        public static void generatet()
+        public static void BFS(int Time)
         {
-            timeFirst.Add(City.start, 0);
-            while (timeFirst.Count < City.cityCollection.Count)
+            string[] infected = { City.start };
+            Queue<string> visiting = new Queue<string>();
+            visiting.Enqueue(City.start);
+
+            while (visiting.Count > 0)
             {
-                foreach (KeyValuePair<KeyValuePair<string, string>, float> pair in val)
+                string nowVisit = visiting.Dequeue();
+                foreach(string newVisit in cityEdge[nowVisit])
                 {
-                    if (timeFirst.ContainsKey(pair.Key.Key))
+                    visiting.Enqueue(newVisit);
+                    if (S(nowVisit, newVisit,Time) > 1)
                     {
-                        int i = 0;
-                        while (S(pair.Key.Key, pair.Key.Value) <= 1)
+                        infected.Append(newVisit);
+                        int tFirst = Time;
+                        while (S(nowVisit, newVisit, tFirst) > 1)
                         {
-                            i++;
+                            tFirst--;
                         }
-                        if (timeFirst.ContainsKey(pair.Key.Value) && timeFirst[pair.Key.Value]>i)
-                        {
-                            timeFirst[pair.Key.Value] = i;
-                        }
-                        else
-                        {
-                            timeFirst.Add(pair.Key.Value, i);
-                        }
+                        timeFirst[newVisit] = tFirst + 1;
                     }
                 }
             }
         }
 
-        public static int t(string a)
+        public static int T(string a)
         {
             return timeFirst[a];
+        }
+
+        public static int t(string a,int Time)
+        {
+            return Time - T(a);
         }
 
         public static double Tr(string a, string b)
@@ -71,9 +78,9 @@ namespace COVID19
             return nilai;
         }
 
-        public static double S(string a, string b)
+        public static double S(string a, string b,int time)
         {
-            return City.I(a, t(a)) * Tr(a, b);
+            return City.I(a, t(a,time)) * Tr(a, b);
         }
     }
 }
